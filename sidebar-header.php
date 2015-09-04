ceab1fcd1817f92781109075c8331b0567b19446
jackmachin <jack.machin125@gmail.com>
2015-09-04 16:29:23 +0100

    No widgets

--- sidebar-header.php
+++ sidebar-header.php
@@ -1,21 +1,15 @@
-				<div id="sidebar-header" class="sidebar" role="complementary">
+<div id="sidebar-header" class="sidebar" role="complementary">
 
-					<?php if ( is_active_sidebar( 'sidebar-header' ) ) : ?>
+    <?php if ( is_active_sidebar( 'sidebar-header' ) ) : ?>
 
-						<?php dynamic_sidebar( 'sidebar-header' ); ?>
+        <?php dynamic_sidebar( 'sidebar-header' ); ?>
 
-					<?php else : ?>
+    <?php else : ?>
 
-						<?php
-							/*
-							 * This content shows up if there are no widgets defined in the backend.
-							*/
-						?>
+        <div class="no-widgets">
+            <p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'bonestheme' );  ?></p>
+        </div>
 
-						<div class="no-widgets">
-							<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'bonestheme' );  ?></p>
-						</div>
+    <?php endif; ?>
 
-					<?php endif; ?>
-
-				</div>
+</div>
